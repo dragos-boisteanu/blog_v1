cff0c1d76c94ba3d53ff529ce63d462708d171e5
Dragos <dragos360@yahoo.com>
2021-02-24 00:42:57 +0200

    small visual fixes to post header add to reading list link added to post

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -8,6 +8,27 @@
 
     <x-post-header :post="$post" class="post__header--not-link"></x-post-header>
 
+    <div class="reading-list-action"> 
+        @if ($post->isReadLater)
+            <form method="POST" action="{{ route('client-read-later.delete') }}">
+                @csrf
+                @method('DELETE')
+
+                <input type="hidden" name="post_id" value="{{$post->id}}">
+                <button type="submit" class="btn btn-read-later">Remove from read later</button>
+            </form>
+        @else
+    
+            <form method="POST" action="{{ route('client-read-later.store') }}">
+                @csrf
+
+                <input type="hidden" name="post_id" value="{{$post->id}}">
+                <button type="submit" class="btn btn-read-later">Read later</button>
+            </form>
+
+        @endif
+    </div>
+    
     <div class="post__content">
         {!! $post->content !!}
     </div>
