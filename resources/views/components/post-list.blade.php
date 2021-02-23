23e6d5cb77ce9f5a1756b01770cf2955bec454fe
Dragos <dragos360@yahoo.com>
2021-02-23 19:24:12 +0200

    read later button styled

--- resources/views/components/post-list.blade.php
+++ resources/views/components/post-list.blade.php
@@ -3,32 +3,30 @@
         <li class="post" id="{{ $post->id }}">
             <a href="{{ route('post.show', ['category'=>$post->category->name, 'slug'=>$post->slug])}}">
                 <x-post-header :post="$post"></x-post-header>
-            </a>                   
+            </a>         
             <div class="preview">
-               {{ $post->preview }}
-            </div>
-            @if ($post->isReadLater)
-                <div>
+                {{ $post->preview }}
+            </div>          
+            <div class="reading-list-action"> 
+                @if ($post->isReadLater)
                     <form method="POST" action="{{ route('client-read-later.delete') }}">
                         @csrf
                         @method('DELETE')
 
                         <input type="hidden" name="post_id" value="{{$post->id}}">
-                        <button type="submit">Remove from read later</button>
+                        <button type="submit" class="btn btn-read-later">Remove from read later</button>
                     </form>
-                </div>
-            @else
-                <div>
+                @else
+            
                     <form method="POST" action="{{ route('client-read-later.store') }}">
                         @csrf
 
                         <input type="hidden" name="post_id" value="{{$post->id}}">
-                        <button type="submit">Read later</button>
+                        <button type="submit" class="btn btn-read-later">Read later</button>
                     </form>
-                </div>
-            @endif
-            
-           
+        
+                @endif
+            </div>
         </li>
     @endforEach
 </ul>
